94357fbde542ebc765ae9b59e258b7ea0dbc9f65
Sean Anderson <github@seananderson.ca>
2013-04-05 20:07:57 -0700

    Remove interval estimation in predict.rrma for now

--- R/predict.rrma.R
+++ R/predict.rrma.R
@@ -6,8 +6,9 @@
 #' \code{rrma} returns predicted values and error
 #' from robust variance meta-regression models 
 #' 
-#' @details predict.rrma is used to generate new predicted variables and error from a data set.  Note
-#' incorporating study and data point level variation is not yet implemented.
+#' @details predict.rrma is used to generate new predicted variables
+#' and error from a data set. Note that incorporating study and data
+#' point level variation is not yet implemented.
 #'
 #' @references
 #' Hedges, L.V., Tipton, E. & Johnson, M.C. (2010). Robust variance
@@ -19,10 +20,8 @@
 #' @param object A meta-regression object of class \code{rrma}
 #' @param newdata Optional new data frame 
 #' @param se.fit A switch indicating if standard errors are required.
-#' @param interval Type of interval calculation.
 #' @param na.action function determining what should be done with 
 #' missing values in \code{newdata}. The default is to predict \code{NA}.
-#' @param level Tolerance/confidence level
 #' 
 #' @export
 #' @return A vector with predicted values or a data frame with predicted and error values
@@ -32,7 +31,6 @@
 #' data(broad)
 #' m <- rrma(formula = lnorReg ~ d18OresidualMean.cent, data =
 #' broad, study_id = study.ID, var_eff = vlnorReg, rho = 0.5) 
-#' m
 #'
 #' pred <- predict(m, se.fit=TRUE, interval="confidence")
 #' plot(lnorReg ~ d18OresidualMean.cent, data=broad)
@@ -43,9 +41,7 @@
 #' matplot(broad$d18OresidualMean.cent[idx], pred[idx,3:4], 
 #'         type="l",lty=2, add=TRUE, col="black")
 
-predict.rrma <- function(object, newdata=NULL, 
-				se.fit=FALSE, interval=c("none", "confidence", "prediction"),
-				na.action=na.pass, level=0.95) {
+predict.rrma <- function(object, newdata=NULL, se.fit=FALSE, na.action=na.pass) {
 
   ret <- list()
   form <- as.formula(object$call$formula)
@@ -64,18 +60,19 @@ predict.rrma <- function(object, newdata=NULL,
   	
   se.fit <- diag(as.matrix(X) %*% tcrossprod(vcov(object), X))
   	
-  if("none" %in% interval) return(data.frame(fit = pred, se.fit = se.fit))
+  #if("none" %in% interval) 
+    return(data.frame(fit = pred, se.fit = se.fit))
   	
-  tfrac <- qt(1-(1-level)/2, nrow(object$input_data))
-  lwr <- pred - tfrac* sqrt(se.fit)
-  upr <- pred + tfrac* sqrt(se.fit)
+  #tfrac <- qt(1-(1-level)/2, nrow(object$input_data))
+  #lwr <- pred - tfrac* sqrt(se.fit)
+  #upr <- pred + tfrac* sqrt(se.fit)
     
-  if("prediction" %in% interval) {
-    lwr <- lwr - tfrac*sqrt(object$tau_sq_est)[1,1] #added matrix notation to avoid odd error
-    upr <- upr + tfrac*sqrt(object$tau_sq_est)[1,1]
+  #if("prediction" %in% interval) {
+    #lwr <- lwr - tfrac*sqrt(object$tau_sq_est)[1,1] #added matrix notation to avoid odd error
+    #upr <- upr + tfrac*sqrt(object$tau_sq_est)[1,1]
     	
-   }
+   #}
     
-   return(data.frame(fit = pred, se.fit = se.fit, lwr = lwr, upr = upr))
+   #return(data.frame(fit = pred, se.fit = se.fit, lwr = lwr, upr = upr))
 }
 
